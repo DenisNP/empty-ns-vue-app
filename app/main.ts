2c764a2e112d645e120ecd63746eaf99dba42ad8
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-24 00:46:17 +0300

    errors fixes

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'nativescript-vue'
-import App from './components/App'
-
+import App from './components/App.vue'
+import store from './store'
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
